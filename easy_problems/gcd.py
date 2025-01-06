d16547a070cfbcdb1821c65c75ab1aee02f6531c
akash2003git <akash.tayade.dev@gmail.com>
2025-01-06 23:44:15 +0530

    Updates

--- easy_problems/gcd.py
+++ easy_problems/gcd.py
@@ -1,1 +1,51 @@
+# Time Complexity: O(min(a,b)) 
+# Auxiliary Space: O(1)
+# def gcd(a, b):
+#     result = min(a, b)
+#     while True:
+#         if a % result == 0 and b % result == 0:
+#             break
+#         result -=1
+#     return result
+
+# Euclidean Algorithm
+# Time Complexity: O(min(a,b))
+# Auxiliary Space: O(min(a,b))
+# def gcd(a ,b):
+#     if a == 0:
+#         return b
+#     if b == 0:
+#         return a
+#     
+#     if a == b:
+#         return a
+#
+#     if a > b:
+#         return gcd(a-b, b)
+#     return gcd(a, b-a)
+
+# Optimized Euclidean
+def gcd(a ,b):
+    if a == 0:
+        return b
+    if b == 0:
+        return a
+    
+    if a == b:
+        return a
+
+    if a > b:
+        if a % b == 0:
+            return b
+        return gcd(a - b, b)
+    else:
+        if b % a == 0:
+            return a
+        return gcd(a, b - a)
+    
+
+print(gcd(36, 60))
+print(gcd(20, 28))
+print(gcd(98, 56))
+
 
